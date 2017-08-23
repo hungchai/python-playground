2a8f3a3e4f60574c1b66c6ebb1cfbb2031ee7ad8
Tom ma <hungchai.nodejs@gmail.com>
2017-08-23 11:16:46 +0000

    add graph to multi-classication/tensor_test.py

--- tensorflow-practice/multi-classication/tensor_test.py
+++ tensorflow-practice/multi-classication/tensor_test.py
@@ -30,38 +30,45 @@ input_data = iris.data.astype(np.float32)
 labels = (np.arange(num_labels) == np.array(iris.target)[:,None]).astype(np.float32)
 
 print(input_data.shape, labels.shape)
-
-xs = tf.placeholder(tf.float32, [None, 4])
-
-ys = tf.placeholder(tf.float32, [None, 3])
-
-l1 = add_layer(xs, 4, 30, activation_func=tf.nn.softmax)
-
-# add output layer
-prediction = add_layer(l1, 30, 3, activation_func=None)
-
 x_data = input_data
-
 y_data = labels
 
-loss = tf.reduce_mean(tf.squared_difference(prediction, ys))
-#loss = tf.reduce_mean(
-#    tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=ys))
 
-train_step = tf.train.AdamOptimizer(0.1).minimize(loss)
-
-init = tf.global_variables_initializer()
 #sess = tf.Session()
-sess = tf.InteractiveSession()
-sess.run(init)
-saver = tf.train.Saver()
 graph = tf.Graph()
+#create input and output tf feed
 
-for step in range(10000):
-    _,prediction_val,loss_val= sess.run([train_step,prediction,loss], feed_dict={xs: x_data, ys: y_data})
-    print(loss.eval(feed_dict={xs: x_data, ys: y_data},session=sess))
+with graph.as_default():
+    xs = tf.placeholder(tf.float32, [None, 4])
+    ys = tf.placeholder(tf.float32, [None, 3])
+        
+    # add hidden layer
+    l1 = add_layer(xs, 4, 30, activation_func=tf.nn.softmax)
+    
+    # add output layer
+    prediction = add_layer(l1, 30, 3, activation_func=None)
 
-#test
-prediction_val= sess.run(prediction, feed_dict={xs: x_data})
-np.savetxt("prediction_answer.csv", prediction_val, delimiter=",")
-np.savetxt("model_answer.csv", y_data, delimiter=",")
+    # calculate loss
+    loss = tf.reduce_mean(tf.squared_difference(prediction, ys))
+    # loss = tf.reduce_mean(
+    #    tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=ys))
+    
+    # set train function
+    train_step = tf.train.AdamOptimizer(0.1).minimize(loss)
+    
+# init = tf.global_variables_initializer()
+# sess = tf.InteractiveSession(graph=graph)
+# sess.run(init)
+
+
+with tf.Session(graph=graph) as sess:
+    tf.global_variables_initializer().run()
+    saver = tf.train.Saver()
+    for step in range(10000):
+        _,prediction_val,loss_val= sess.run([train_step,prediction,loss], feed_dict={xs: x_data, ys: y_data})
+        print(loss.eval(feed_dict={xs: x_data, ys: y_data},session=sess))
+
+    #test
+    prediction_val= sess.run(prediction, feed_dict={xs: x_data})
+    np.savetxt("prediction_answer.csv", prediction_val, delimiter=",")
+    np.savetxt("model_answer.csv", y_data, delimiter=",")
