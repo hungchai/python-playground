88a65c1499e94b657f7eb65ed33836e724383e01
Tom ma <hungchai08@yahoo.com.hk>
2017-12-30 00:28:41 +0800

    write

--- stock-predict/run.py
+++ stock-predict/run.py
@@ -5,11 +5,15 @@ from subprocess import check_output
 from keras.layers.core import Dense, Activation, Dropout
 from keras.layers.recurrent import LSTM
 from keras.models import Sequential
-from sklearn.cross_validation import  train_test_split
+from sklearn.model_selection import train_test_split
 import time #helper libraries
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 from numpy import newaxis
 
-prices_dataset =  pd.read_csv('../input/prices.csv', header=0)
-prices_dataset
+prices_dataset =  pd.read_csv('./input/prices.csv', header=0)
+
+yahoo = prices_dataset[prices_dataset['symbol']=='YHOO']
+yahoo_stock_prices = yahoo.close.values.astype('float32')
+yahoo_stock_prices2 = yahoo_stock_prices.reshape(yahoo_stock_prices.shape[0], 1)
+yahoo_stock_prices2.shape
