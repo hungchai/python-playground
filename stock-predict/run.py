813ecd8af75a76fa8f89660eedaf8731cc05d576
Tom ma <hungchai08@yahoo.com.hk>
2018-01-01 00:09:47 +0800

    complete 1/5

--- stock-predict/run.py
+++ stock-predict/run.py
@@ -15,5 +15,20 @@ prices_dataset =  pd.read_csv('./input/prices.csv', header=0)
 
 yahoo = prices_dataset[prices_dataset['symbol']=='YHOO']
 yahoo_stock_prices = yahoo.close.values.astype('float32')
-yahoo_stock_prices2 = yahoo_stock_prices.reshape(yahoo_stock_prices.shape[0], 1)
-yahoo_stock_prices2.shape
+yahoo_stock_prices = yahoo_stock_prices.reshape(yahoo_stock_prices.shape[0], 1)
+
+plt.figure(1)
+plt.subplot(211)
+plt.plot(yahoo_stock_prices)
+
+scaler = MinMaxScaler(feature_range=(0, 1))
+yahoo_stock_prices = scaler.fit_transform(yahoo_stock_prices)
+
+plt.subplot(212)
+plt.plot(yahoo_stock_prices)
+plt.show()
+
+train_size = int(len(yahoo_stock_prices) * 0.80)
+test_size = len(yahoo_stock_prices) - train_size
+train, test = yahoo_stock_prices[0:train_size,:], yahoo_stock_prices[train_size:len(yahoo_stock_prices),:]
+print(len(train), len(test))
