703eacc3a697a2bca369b33972b78ccda75f452d
Tom ma <hungchai08@yahoo.com.hk>
2018-01-03 00:11:20 +0800

    complete 2/5

--- stock-predict/run.py
+++ stock-predict/run.py
@@ -26,9 +26,29 @@ yahoo_stock_prices = scaler.fit_transform(yahoo_stock_prices)
 
 plt.subplot(212)
 plt.plot(yahoo_stock_prices)
-plt.show()
+# plt.show()
 
 train_size = int(len(yahoo_stock_prices) * 0.80)
 test_size = len(yahoo_stock_prices) - train_size
 train, test = yahoo_stock_prices[0:train_size,:], yahoo_stock_prices[train_size:len(yahoo_stock_prices),:]
 print(len(train), len(test))
+
+
+# convert an array of values into a dataset matrix
+def create_dataset(dataset, look_back=1):
+	dataX, dataY = [], []
+	for i in range(len(dataset)-look_back-1):
+		a = dataset[i:(i+look_back), 0]
+		dataX.append(a)
+		dataY.append(dataset[i + look_back, 0])
+	return np.array(dataX), np.array(dataY)
+
+
+# reshape into X=t and Y=t+1
+look_back = 1
+trainX, trainY = create_dataset(train, look_back)
+testX, testY = create_dataset(test, look_back)
+
+trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+
